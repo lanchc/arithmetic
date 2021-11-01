1d1469cbae81388854c336004b29de7772733200
懒虫 <11534516+lanchc@users.noreply.github.com>
2021-11-01 16:24:07 +0800

    11

--- DocxHelper.py
+++ DocxHelper.py
@@ -120,7 +120,7 @@ if __name__ == '__main__':
     checkup_dir(project_root_dir() + '/dist/')
 
     # 构建文件份数
-    documents_count = 5
+    documents_count = 50
 
     print("文档操作助手")
     for i in range(1, documents_count + 1):
